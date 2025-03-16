3cbe0dcd1f40331776b20eedad6badfa8278619f
changoug <tien.chang@mail.utoronto.ca>
2025-03-15 21:49:39 -0400

    finished roadmaps page

--- ai-tutor/src/app/page.tsx
+++ ai-tutor/src/app/page.tsx
@@ -44,25 +44,29 @@ export default function Home() {
 >>>>>>> 7f05b89 (Implemented course selection for query on landing page)
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       MAIN PAGE
       <AuthForm setToken={setToken} />
 =======
     <div className="grid flex flex-col pt-[100] place-content-center">
+=======
+    <div className="grid flex flex-col pt-[20vh] place-content-center">
+>>>>>>> f973dad (finished roadmaps page)
         <div className="relative text-5xl w-fit mx-auto">
           <div className="absolute left-0 right-0 top-1/2 -translate-y-1/2 h-8 -mx-4 bg-[#E9F3DA]"></div>
           <div className="relative text-7xl">ADVISORY</div>
         </div>
         <div className="text-md text-center m-2">A Personal AI Tutor.</div>
           {/* Input Box */}
-        <div className="relative flex flex-row max-w-7xl w-[60vw] p-4 border-none rounded-full bg-[#FFF0D2]">
+        <div className="text-md relative flex flex-row max-w-7xl w-[60vw] p-3 border-none rounded-full bg-[#FFF0D2]">
             {/* Input Field */}
             <div className="relative inline-flex w-fit ml-4">
               <LandingDropdown availableCourses={courses} currCourse={selectedCourse} selectCourse={setSelectedCourse} />
             </div>
-            <div className= "relative flex items-center p-1 text-2xl text-gray-400">|</div>
+            <div className= "relative flex p-1 text-2xl mb-1 text-gray-400">|</div>
             <input
-              className="relative flex items-center bg-transparent w-full focus:outline-none placeholder:text-gray-400"
+              className="relative flex bg-transparent w-full focus:outline-none placeholder:text-gray-400"
               type="text"
               placeholder="Ask me something..."
               // value={input}
@@ -70,7 +74,7 @@ export default function Home() {
               onKeyDown={ (e) => e.key === "Enter" && router.push(`/chatbot?course=${selectedCourse}&query=${(e.target as HTMLTextAreaElement).value}`) }
             />
             {/* Send Button (Inside Input) */}
-            <button className="text-3xl ml-3 text-gray-400">➤</button>
+            <button className="text-3xl ml-3 mr-3 text-gray-400">➤</button>
         </div>
 >>>>>>> 4d8430d (added basic middleware for passing path, basic landing page implemented)
     </div>
--- ai-tutor/src/app/roadmaps/page.tsx
+++ ai-tutor/src/app/roadmaps/page.tsx
@@ -22,12 +22,19 @@ export default function Roadmaps() {
   }, []);
 
   return (
-    <div>
-      <div>Roadmaps</div>
-      <div>
-
+    <div className="grid pt-[5vh] place-content-center">
+      <div className="relative relative w-fit mx-auto">
+        <div className="absolute left-0 right-0 top-1/2 -translate-y-1/2 h-8 -mx-4 bg-[#E9F3DA]"></div>
+        <div className="relative text-7xl">ROADMAPS</div>
       </div>
-
+      <div className="w-fit max-w-5xl grid h-100 grid-cols-4 gap-4 pt-10">
+        {courses.map( (c) => {
+          return (
+            <div key={c} className="w-[200px] h-[100px] shadow-sm place-content-center text-center rounded-lg bg-[#FFF0D2]">{c}</div>
+          );
+        })
+        }
+      </div>
     </div>
   );
 }
